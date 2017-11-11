281e34f2aa608e7cfeb4ea0a33dfe4b43c51c4ce
jose <jose@HP-Ubuntu>
2017-11-11 11:58:50 +0100

    Testing Ubuntu...

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <!meta charset="utf-8">
   <!meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <title>Dolphin-PHP on OpenShift...</title>
+  <title>Testing on UBUNTU...</title>
 
   <!Inserta las Hojas de estilo en cascada...>
 
